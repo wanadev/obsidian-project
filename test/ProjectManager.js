78bddcf9b36033ca5b99958d7e6a96d29ba7ecf4
Fabien LOISON <flo@flogisoft.com>
2015-12-01 11:40:46 +0100

    new empty project

--- test/ProjectManager.js
+++ test/ProjectManager.js
@@ -5,44 +5,68 @@ var ProjectManager = require("../lib/ProjectManager.js");
 
 describe("ProjectManager", function() {
 
-    it("has the right default mimetype and file extention", function() {
-        var project = new ProjectManager();
-        expect(project.mimetype).to.equal("application/x-wanadev-project");
-        expect(project.fileExt).to.equal("wprj");
+    describe("MISC", function() {
 
-        var project2 = new ProjectManager({
-            mimetype: "application/x-test-mime",
-            fileExt: "test"
+        it("has the right default mimetype and file extention", function() {
+            var project = new ProjectManager();
+            expect(project.mimetype).to.equal("application/x-wanadev-project");
+            expect(project.fileExt).to.equal("wprj");
+
+            var project2 = new ProjectManager({
+                mimetype: "application/x-test-mime",
+                fileExt: "test"
+            });
+            expect(project2.mimetype).to.equal("application/x-test-mime");
+            expect(project2.fileExt).to.equal("test");
         });
-        expect(project2.mimetype).to.equal("application/x-test-mime");
-        expect(project2.fileExt).to.equal("test");
+
+        it("can read and write WPRJ options", function(done) {
+            var project = new ProjectManager();
+
+            expect(project.wprjOptions).to.eql({type: "GENERIC", metadataFormat: 1, projectFormat: 1, blobIndexFormat: 1});
+
+            project.wprjOptions = {type: "TESTTYPE"};
+            expect(project.wprjOptions).to.eql({type: "TESTTYPE", metadataFormat: 1, projectFormat: 1, blobIndexFormat: 1});
+
+            project.wprjOptions.metadataFormat = 0;
+            setTimeout(function() {
+                expect(project.wprjOptions).to.eql({type: "TESTTYPE", metadataFormat: 0, projectFormat: 1, blobIndexFormat: 1});
+                done();
+            }, 0);
+        });
+
+        it("can read and write WPRJ metadata", function () {
+            var project = new ProjectManager();
+
+            expect(project.metadata).to.eql({});
+
+            project.metadata = {data1: "foo"};
+            expect(project.metadata).to.eql({data1: "foo"});
+
+            project.metadata.data2 = "bar";
+            expect(project.metadata).to.eql({data1: "foo", data2: "bar"});
+        });
+
     });
 
-    it("can read and write WPRJ options", function(done) {
-        var project = new ProjectManager();
+    describe("NEW PROJECT", function() {
 
-        expect(project.wprjOptions).to.eql({type: "GENERIC", metadataFormat: 1, projectFormat: 1, blobIndexFormat: 1});
+        it("can create new empty projects", function() {
+            var project = new ProjectManager({
+                metadata: {
+                    test: "foo"
+                },
+                wprjOptions: {
+                    type: "FOOBARBAZ"
+                }
+            });
+            expect(project.metadata).to.eql({test: "foo"});
+            expect(project.wprjOptions.type).to.equal("FOOBARBAZ");
 
-        project.wprjOptions = {type: "TESTTYPE"};
-        expect(project.wprjOptions).to.eql({type: "TESTTYPE", metadataFormat: 1, projectFormat: 1, blobIndexFormat: 1});
-
-        project.wprjOptions.metadataFormat = 0;
-        setTimeout(function() {
-            expect(project.wprjOptions).to.eql({type: "TESTTYPE", metadataFormat: 0, projectFormat: 1, blobIndexFormat: 1});
-            done();
-        }, 0);
-    });
-
-    it("can read and write WPRJ metadata", function () {
-        var project = new ProjectManager();
-
-        expect(project.metadata).to.eql({});
-
-        project.metadata = {data1: "foo"};
-        expect(project.metadata).to.eql({data1: "foo"});
-
-        project.metadata.data2 = "bar";
-        expect(project.metadata).to.eql({data1: "foo", data2: "bar"});
+            project.newEmptyProject({hello: "world"});
+            expect(project.metadata).to.eql({hello: "world"});
+            expect(project.wprjOptions.type).to.equal("FOOBARBAZ");
+        });
     });
 
 });
