bc982476a69ad7a46b22cee3ee0191341075336c
Fabien LOISON <flo@flogisoft.com>
2015-12-03 17:57:00 +0100

    add sourcemap for browserified files...

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,11 @@ module.exports = function(grunt) {
                 files: {
                     "test/browser/tests.generated.js": ["test/*.js"]
                 }
+            },
+            options: {
+                browserifyOptions: {
+                    debug: true
+                }
             }
         },
 
