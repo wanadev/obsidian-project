34d4a4ba98a6f61671ea47e5a0fc8ae7f4ccbb56
Alexis Breust <abreust@wanadev.fr>
2016-09-09 10:50:52 +0200

    Added tests for blobs in history

--- test/History.js
+++ test/History.js
@@ -23,17 +23,33 @@ var OwnStructure = Structure.$extend({
 
 Structure.$register(OwnStructure);
 
-var defaultPM = new ProjectManager();
-defaultPM.addStructure(new OwnStructure({ x: "0_0" }), "l0");
-defaultPM.addStructure(new OwnStructure({ x: "0_1" }), "l0");
-defaultPM.addStructure(new OwnStructure({ x: "0_2" }), "l0");
-defaultPM.addStructure(new OwnStructure({ x: "1_0" }), "l1");
-defaultPM.addStructure(new OwnStructure({ x: "1_1" }), "l1");
-defaultPM.addStructure(new OwnStructure({ x: "2_0" }), "l2");
+var project;
+var defaultProject = new ProjectManager();
+defaultProject.addStructure(new OwnStructure({ x: "0_0" }), "l0");
+defaultProject.addStructure(new OwnStructure({ x: "0_1" }), "l0");
+defaultProject.addStructure(new OwnStructure({ x: "0_2" }), "l0");
+defaultProject.addStructure(new OwnStructure({ x: "1_0" }), "l1");
+defaultProject.addStructure(new OwnStructure({ x: "1_1" }), "l1");
+defaultProject.addStructure(new OwnStructure({ x: "2_0" }), "l2");
 
-var pm;
-pm = new ProjectManager();
-pm.unserialize(defaultPM.serialize());
+var buffer = new Buffer([
+    0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00, 0x0d,
+    0x49, 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x10,
+    0x02, 0x03, 0x00, 0x00, 0x00, 0x62, 0x9d, 0x17, 0xf2, 0x00, 0x00, 0x00,
+    0x09, 0x50, 0x4c, 0x54, 0x45, 0x00, 0x00, 0x00, 0xff, 0x00, 0x00, 0xff,
+    0xff, 0xff, 0x67, 0x19, 0x64, 0x1e, 0x00, 0x00, 0x00, 0x01, 0x62, 0x4b,
+    0x47, 0x44, 0x00, 0x88, 0x05, 0x1d, 0x48, 0x00, 0x00, 0x00, 0x09, 0x70,
+    0x48, 0x59, 0x73, 0x00, 0x00, 0x0e, 0xc4, 0x00, 0x00, 0x0e, 0xc4, 0x01,
+    0x95, 0x2b, 0x0e, 0x1b, 0x00, 0x00, 0x00, 0x07, 0x74, 0x49, 0x4d, 0x45,
+    0x07, 0xdf, 0x0b, 0x12, 0x0d, 0x0b, 0x17, 0xca, 0x83, 0x65, 0x00, 0x00,
+    0x00, 0x00, 0x3d, 0x49, 0x44, 0x41, 0x54, 0x08, 0x1d, 0x0d, 0xc1, 0xc1,
+    0x0d, 0x00, 0x21, 0x0c, 0x03, 0xc1, 0x2d, 0x07, 0xd1, 0x0f, 0xfd, 0x9c,
+    0xf2, 0x8a, 0x5c, 0x05, 0xf2, 0x0b, 0xa5, 0xca, 0xf3, 0x0c, 0x27, 0x98,
+    0xe0, 0xf3, 0x15, 0x6e, 0x15, 0x2e, 0x0b, 0xeb, 0x09, 0xdf, 0x32, 0x13,
+    0x4c, 0x50, 0x7a, 0x43, 0xeb, 0x0d, 0xa5, 0xb5, 0xe9, 0x6e, 0x51, 0x5a,
+    0x9b, 0x09, 0x4e, 0xfc, 0x91, 0x4d, 0x22, 0x7f, 0x72, 0xcc, 0xb0, 0x7f,
+    0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4e, 0x44, 0xae, 0x42, 0x60, 0x82
+]);
 
 function expectLayer(layer1, layer2) {
     expect(layer1).to.have.length(layer2.length);
@@ -47,25 +63,25 @@ function expectLayer(layer1, layer2) {
 describe("History", function() {
 
     beforeEach(function() {
-        pm = new ProjectManager();
-        pm.unserialize(defaultPM.serialize());
+        project = new ProjectManager();
+        project.unserialize(defaultProject.serialize());
     });
 
     describe("LENGTH", function() {
 
         it("can set max length", function() {
-            var history = new History(pm);
+            var history = new History(project);
             expect(history.maxLength).to.equal(0);
 
             history.maxLength = 5;
             expect(history.maxLength).to.equal(5);
 
-            var history2 = new History(pm, { maxLength: 3 });
+            var history2 = new History(project, { maxLength: 3 });
             expect(history2.maxLength).to.equal(3);
         });
 
         it("is increased up to max length", function() {
-            var history = new History(pm, { maxLength: 3 });
+            var history = new History(project, { maxLength: 3 });
             expect(history.length).to.equal(0);
 
             history.snapshot();
@@ -78,7 +94,7 @@ describe("History", function() {
         });
 
         it("keeps max length the same", function() {
-            var history = new History(pm, { maxLength: 3 });
+            var history = new History(project, { maxLength: 3 });
             expect(history.maxLength).to.equal(3);
 
             history.snapshot();
@@ -91,7 +107,7 @@ describe("History", function() {
         });
 
         it("removes old snapshots when max length is changed", function() {
-            var history = new History(pm, { maxLength: 3 });
+            var history = new History(project, { maxLength: 3 });
             expect(history.maxLength).to.equal(3);
             history.snapshot();
             history.snapshot();
@@ -104,102 +120,102 @@ describe("History", function() {
 
     });
 
-    describe("NAVIGATING", function() {
+    describe("STRUCTURES", function() {
 
         it("can go back", function() {
-            var history = new History(pm, { maxLength: 5 });
+            var history = new History(project, { maxLength: 5 });
             history.snapshot();
 
-            pm.layers.l1[0].x = "1_0b";
+            project.layers.l1[0].x = "1_0b";
             history.snapshot();
 
-            expect(pm.layers).to.only.have.keys("l0", "l1", "l2");
-            expectLayer(pm.layers.l0, defaultPM.layers.l0);
-            expect(pm.layers.l1).to.have.length(defaultPM.layers.l1.length);
-            expect(pm.layers.l1[0].x).to.be.equal("1_0b");
-            expect(pm.layers.l1[1].x).to.be.eql(defaultPM.layers.l1[1].x);
-            expectLayer(pm.layers.l2, defaultPM.layers.l2);
+            expect(project.layers).to.only.have.keys("l0", "l1", "l2");
+            expectLayer(project.layers.l0, defaultProject.layers.l0);
+            expect(project.layers.l1).to.have.length(defaultProject.layers.l1.length);
+            expect(project.layers.l1[0].x).to.be.equal("1_0b");
+            expect(project.layers.l1[1].x).to.be.eql(defaultProject.layers.l1[1].x);
+            expectLayer(project.layers.l2, defaultProject.layers.l2);
 
             history.back();
-            expectLayer(pm.layers.l0, defaultPM.layers.l0);
-            expectLayer(pm.layers.l1, defaultPM.layers.l1);
-            expectLayer(pm.layers.l2, defaultPM.layers.l2);
+            expectLayer(project.layers.l0, defaultProject.layers.l0);
+            expectLayer(project.layers.l1, defaultProject.layers.l1);
+            expectLayer(project.layers.l2, defaultProject.layers.l2);
         });
 
         it("can go back and forth", function() {
-            var history = new History(pm, { maxLength: 5 });
+            var history = new History(project, { maxLength: 5 });
             history.snapshot();
 
-            pm.layers.l1[0].x = "1_0b";
+            project.layers.l1[0].x = "1_0b";
             history.snapshot();
 
             history.back();
             history.forward();
-            expectLayer(pm.layers.l0, defaultPM.layers.l0);
-            expect(pm.layers.l1).to.have.length(defaultPM.layers.l1.length);
-            expect(pm.layers.l1[0].x).to.be.equal("1_0b");
-            expect(pm.layers.l1[1].x).to.be.eql(defaultPM.layers.l1[1].x);
-            expectLayer(pm.layers.l2, defaultPM.layers.l2);
+            expectLayer(project.layers.l0, defaultProject.layers.l0);
+            expect(project.layers.l1).to.have.length(defaultProject.layers.l1.length);
+            expect(project.layers.l1[0].x).to.be.equal("1_0b");
+            expect(project.layers.l1[1].x).to.be.eql(defaultProject.layers.l1[1].x);
+            expectLayer(project.layers.l2, defaultProject.layers.l2);
         });
 
         it("won't go too far", function() {
-            var history = new History(pm, { maxLength: 5 });
+            var history = new History(project, { maxLength: 5 });
             history.snapshot();
-            pm.layers.l1[0].x = "1_0b";
+            project.layers.l1[0].x = "1_0b";
             history.snapshot();
 
             history.back();
             history.back(); // Too far!
-            expectLayer(pm.layers.l0, defaultPM.layers.l0);
-            expectLayer(pm.layers.l1, defaultPM.layers.l1);
-            expectLayer(pm.layers.l2, defaultPM.layers.l2);
+            expectLayer(project.layers.l0, defaultProject.layers.l0);
+            expectLayer(project.layers.l1, defaultProject.layers.l1);
+            expectLayer(project.layers.l2, defaultProject.layers.l2);
             expect(history.try(-5)).to.be.equal(0);
             expect(history.try(5)).to.be.equal(1);
 
             history.forward();
-            expectLayer(pm.layers.l0, defaultPM.layers.l0);
-            expect(pm.layers.l1).to.have.length(defaultPM.layers.l1.length);
-            expect(pm.layers.l1[0].x).to.be.equal("1_0b");
-            expect(pm.layers.l1[1].x).to.be.eql(defaultPM.layers.l1[1].x);
-            expectLayer(pm.layers.l2, defaultPM.layers.l2);
+            expectLayer(project.layers.l0, defaultProject.layers.l0);
+            expect(project.layers.l1).to.have.length(defaultProject.layers.l1.length);
+            expect(project.layers.l1[0].x).to.be.equal("1_0b");
+            expect(project.layers.l1[1].x).to.be.eql(defaultProject.layers.l1[1].x);
+            expectLayer(project.layers.l2, defaultProject.layers.l2);
             expect(history.try(-5)).to.be.equal(-1);
             expect(history.try(5)).to.be.equal(0);
 
             history.go(-2); // Too far!
-            expectLayer(pm.layers.l0, defaultPM.layers.l0);
-            expectLayer(pm.layers.l1, defaultPM.layers.l1);
-            expectLayer(pm.layers.l2, defaultPM.layers.l2);
+            expectLayer(project.layers.l0, defaultProject.layers.l0);
+            expectLayer(project.layers.l1, defaultProject.layers.l1);
+            expectLayer(project.layers.l2, defaultProject.layers.l2);
             expect(history.try(-5)).to.be.equal(0);
             expect(history.try(5)).to.be.equal(1);
         });
 
         it("deletes old snapshots automatically", function() {
-            var history = new History(pm, { maxLength: 5 });
-            pm.layers.l1[0].x = "1_0_1";
+            var history = new History(project, { maxLength: 5 });
+            project.layers.l1[0].x = "1_0_1";
             history.snapshot();
-            pm.layers.l1[0].x = "1_0_2";
+            project.layers.l1[0].x = "1_0_2";
             history.snapshot();
-            pm.layers.l1[0].x = "1_0_3";
+            project.layers.l1[0].x = "1_0_3";
             history.snapshot();
-            pm.layers.l1[0].x = "1_0_4";
+            project.layers.l1[0].x = "1_0_4";
             history.snapshot();
-            pm.layers.l1[0].x = "1_0_5";
+            project.layers.l1[0].x = "1_0_5";
             history.snapshot();
-            pm.layers.l1[0].x = "1_0_6";
+            project.layers.l1[0].x = "1_0_6";
             history.snapshot();
-            pm.layers.l1[0].x = "1_0_7";
+            project.layers.l1[0].x = "1_0_7";
             history.snapshot();
 
             history.go(-4);
-            expectLayer(pm.layers.l0, defaultPM.layers.l0);
-            expect(pm.layers.l1[0].x).to.be.equal("1_0_3");
-            expect(pm.layers.l1[1].x).to.be.eql(defaultPM.layers.l1[1].x);
-            expectLayer(pm.layers.l2, defaultPM.layers.l2);
+            expectLayer(project.layers.l0, defaultProject.layers.l0);
+            expect(project.layers.l1[0].x).to.be.equal("1_0_3");
+            expect(project.layers.l1[1].x).to.be.eql(defaultProject.layers.l1[1].x);
+            expectLayer(project.layers.l2, defaultProject.layers.l2);
             expect(history.try(-1)).to.be.equal(0);
         });
 
         it("deletes branched snapshots automatically", function() {
-            var history = new History(pm, { maxLength: 5 });
+            var history = new History(project, { maxLength: 5 });
             history.snapshot();
             history.snapshot();
             history.snapshot();
@@ -210,9 +226,9 @@ describe("History", function() {
             expect(history.length).to.be(4);
 
             history.snapshot();
-            expectLayer(pm.layers.l0, defaultPM.layers.l0);
-            expectLayer(pm.layers.l1, defaultPM.layers.l1);
-            expectLayer(pm.layers.l2, defaultPM.layers.l2);
+            expectLayer(project.layers.l0, defaultProject.layers.l0);
+            expectLayer(project.layers.l1, defaultProject.layers.l1);
+            expectLayer(project.layers.l2, defaultProject.layers.l2);
             expect(history.length).to.be(3);
             expect(history.try(1)).to.be(0);
         });
@@ -222,27 +238,75 @@ describe("History", function() {
     describe("ORDER", function() {
 
         it("moves structures back to their correct layer", function() {
-            var history = new History(pm, { maxLength: 5 });
+            var history = new History(project, { maxLength: 5 });
             history.snapshot();
-            pm.setStructureLayer(pm.layers.l0[0], "l3");
+            project.setStructureLayer(project.layers.l0[0], "l3");
             history.snapshot();
 
             history.back();
-            expectLayer(pm.layers.l0, defaultPM.layers.l0);
-            expectLayer(pm.layers.l1, defaultPM.layers.l1);
-            expectLayer(pm.layers.l2, defaultPM.layers.l2);
+            expectLayer(project.layers.l0, defaultProject.layers.l0);
+            expectLayer(project.layers.l1, defaultProject.layers.l1);
+            expectLayer(project.layers.l2, defaultProject.layers.l2);
         });
 
         it("moves structures within its layer", function() {
-            var history = new History(pm, { maxLength: 5 });
+            var history = new History(project, { maxLength: 5 });
             history.snapshot();
-            pm.setStructureIndex(pm.layers.l0[0], 1);
+            project.setStructureIndex(project.layers.l0[0], 1);
             history.snapshot();
 
             history.back();
-            expectLayer(pm.layers.l0, defaultPM.layers.l0);
-            expectLayer(pm.layers.l1, defaultPM.layers.l1);
-            expectLayer(pm.layers.l2, defaultPM.layers.l2);
+            expectLayer(project.layers.l0, defaultProject.layers.l0);
+            expectLayer(project.layers.l1, defaultProject.layers.l1);
+            expectLayer(project.layers.l2, defaultProject.layers.l2);
+        });
+
+    });
+
+    describe("BLOBS", function() {
+
+        it.skip("can revert blobs", function() {
+            var history = new History(project, { maxLength: 5 });
+            history.snapshot();
+
+            var id = project.addBlobFromBuffer(buffer);
+            expect(project.$data.wprjFile.getBlob(id)).to.be(buffer);
+            history.snapshot();
+
+            history.back();
+            expect(project.$data.wprjFile.getBlob(id)).to.be(null);
+
+            history.forward();
+            expect(project.$data.wprjFile.getBlob(id)).to.be(buffer);
+
+            project.removeBlob(id);
+            expect(project.$data.wprjFile.getBlob(id)).to.be(null);
+            history.snapshot();
+
+            history.back();
+            expect(project.$data.wprjFile.getBlob(id)).to.be(buffer);
+        });
+
+        it.skip("closes old blobs automatically", function() {
+            var history = new History(project, { maxLength: 2 });
+            var id = project.addBlobFromBuffer(buffer);
+            var blob = project.getBlob(id);
+            history.snapshot();
+
+            project.removeBlob(id);
+            history.snapshot();
+            expect(blob.isClosed).to.be(false);
+
+            history.snapshot();
+            expect(blob.isClosed).to.be(true);
+        });
+
+        it.skip("closes blobs never seen in snapshots", function() {
+            var history = new History(project, { maxLength: 5 });
+            var id = project.addBlobFromBuffer(buffer);
+            var blob = project.getBlob(id);
+            project.removeBlob(id);
+            expect(blob.isClosed).to.be(true);
         });
 
     });
@@ -250,7 +314,7 @@ describe("History", function() {
     describe("CLEAR", function() {
 
         it("removes everything", function() {
-            var history = new History(pm, { maxLength: 5 });
+            var history = new History(project, { maxLength: 5 });
             history.snapshot();
             history.snapshot();
             history.snapshot();
